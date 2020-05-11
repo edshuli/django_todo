f0ea0ce0db442c1d681fd0c5445f7f46fbc1dbb6
Edna <edna_shuli@hotmail.gr>
2020-05-11 11:14:21 +0000

    Chnaged title

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -90,6 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
+         
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
